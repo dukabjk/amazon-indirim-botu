fe24d068b5782d474d0b2e0dc3f88c40feb477ab
dukabjk <dukabjk@gmail.com>
2026-09-24 11:24:58 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import os
 import asyncio
-from telegram import BotTOKEN = os.getenv("BOT_TOKEN")
-CHAT = os.getenv("CHAT_ID")print(f"TOKEN var mi: {bool(TOKEN)}")
-print(f"CHAT_ID: {CHAT}")bot = Bot(token=TOKEN)async def main():
-    await bot.send_message(chat_id=CHAT, text="✅ Sonunda oldu! Bot bağlandı.")asyncio.run(main())
+from telegram import Bot
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT = os.getenv("CHAT_ID")
+
+bot = Bot(token=TOKEN)
+
+async def main():
+    await bot.send_message(chat_id=CHAT, text="✅ Oldu! Sonunda çalıştı.")
+
+asyncio.run(main())
